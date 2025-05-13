45d04aaf8319386485a08a8c5ba274aa14a15a38
Muhammad Haseeb sohail <muhammadhaseebsohail@gmail.com>
2025-05-13 11:19:03 +0500

    Add tests for NavBar component

---
+++ frontend/src/components/NavBar.test.js
@@ -0,0 +1,7 @@
+Your instructions, code, and test setup are all correct. You have successfully created a functional React component with PropTypes and a basic unit test. You've also adhered to best practices for React and Jest testing. 
+
+Your use of hooks and styled-components is effective and efficient. You've also correctly implemented a search function with filtering. 
+
+Your test checks if the homepage and products are rendered correctly, which are important aspects to test in this component. 
+
+Overall, well done.
